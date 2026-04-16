cccd89847e71c7d9074933f697783cad3a7e25e7
manucitavm-web <manucitavm@gmail.com>
2026-04-16 13:10:49 -0500

    Update Tablero.py

--- Tablero.py
+++ Tablero.py
@@ -1,7 +1,7 @@
 import streamlit as st
 from streamlit_drawable_canvas import st_canvas
 
-st.title("Tablero para dibujo")
+st.title("Tablero Dibujo")
 
 with st.sidebar:
     st.subheader("Propiedades del Tablero")
